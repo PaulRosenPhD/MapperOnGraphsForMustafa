6415c204ed91dee41b464ae8a40ca52fb25ad156
Paul Rosen <prosen@usf.edu>
2022-09-23 17:39:06 -0600

    fixed menu

--- docs/js/common.js
+++ docs/js/common.js
@@ -28,7 +28,8 @@ function clear_chart( chart_name ){
 	$(chart_name).empty();
 }
 
-var pages = ["small_multiples.html","figures.html", "interactive.html", "user_study.html"];
+//var pages = ["small_multiples.html","figures.html", "interactive.html", "user_study.html"];
+var pages = [];
 var page_titles = {"small_multiples.html": "Small Multiple Parameter Explorer",
                     "figures.html": "Paper Figures",
                     "interactive.html": "Interactive",
